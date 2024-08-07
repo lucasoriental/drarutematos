d485aa4c31c6f33be2c5622a241f6cae86e5e387
Lucas Oriental dos Santos <lucasoriental16@gmail.com>
2024-08-07 19:43:30 +0100

    main
    Adição do texto na área de Sobre mim

--- src/components/sections/aboutMe.jsx
+++ src/components/sections/aboutMe.jsx
@@ -1,4 +1,4 @@
-import tempImg from "../../assets/images/tempImage512.png";
+import fotoDePerfil from "../../assets/images/sobreMim.jpg";
 
 export default function SobreMim() {
   return (
@@ -7,62 +7,12 @@ export default function SobreMim() {
         <p className="container-titulo-da-seccao">Sobre Mim</p>
         <div className="container-sobre-mim-div">
           <img
-            src={tempImg}
+            src={fotoDePerfil}
             alt="Foto de Perfil"
             className="container-sobre-mim-imagem"
           />
           <p className="container-sobre-mim-texto">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            pulvinar mi in leo tincidunt commodo. Mauris non ipsum tempor,
-            egestas nisi pellentesque, pharetra ante. Nam diam neque, tempor in
-            viverra eu, imperdiet non purus. Class aptent taciti sociosqu ad
-            litora torquent per conubia nostra, per inceptos himenaeos. Duis sit
-            amet faucibus felis. Quisque ac arcu viverra, imperdiet velit vel,
-            placerat purus. Ut id sollicitudin neque. Donec semper efficitur
-            tincidunt. Phasellus molestie ut mauris vitae vulputate. Nulla
-            molestie libero id augue congue, non vestibulum ex ornare. Sed eget
-            magna maximus, sollicitudin elit sit amet, convallis erat. Fusce
-            eget mauris ac diam vehicula placerat iaculis quis leo. Morbi sit
-            amet massa ac nibh lacinia posuere. Maecenas volutpat erat pharetra,
-            gravida felis vel, finibus tellus. Morbi sapien ante, pretium et
-            felis et, pellentesque viverra elit. Suspendisse sodales porta
-            lacinia. Sed dictum erat quis orci semper, a rutrum metus facilisis.
-            Proin semper, eros sit amet pulvinar posuere, dolor odio lacinia
-            lacus, sed condimentum nisl tortor sed velit. Aliquam porta lorem
-            sem, in eleifend justo vestibulum eget. Vestibulum sapien turpis,
-            cursus sit amet quam eu, iaculis scelerisque diam. Pellentesque
-            vitae iaculis neque. Sed ultrices laoreet lorem. Donec sagittis odio
-            ut mattis ullamcorper. Vivamus et egestas velit. Vestibulum quis
-            sodales ipsum. In hac habitasse platea dictumst. Mauris ac mollis
-            erat, porttitor tristique erat. Vivamus luctus neque ipsum, in
-            vestibulum diam fringilla eget. Praesent consectetur diam ante, quis
-            consectetur diam sollicitudin nec. Mauris porttitor nisl id mattis
-            mollis. Vestibulum eget felis non nulla posuere bibendum.
-            Pellentesque malesuada tortor vel risus porttitor blandit. Integer
-            in libero tempus, ornare velit at, eleifend enim. Ut eu mauris
-            fermentum, ornare lorem ut, malesuada nibh. Integer accumsan, lacus
-            vel viverra laoreet, velit metus facilisis ante, at hendrerit nulla
-            nisl a sapien. Aenean eu facilisis tortor. Ut consectetur massa nec
-            dolor pharetra, pellentesque ultrices dolor maximus. Suspendisse at
-            elit eu erat faucibus tempor eget id augue. Maecenas libero leo,
-            semper vitae luctus vitae, sagittis non lorem. Aliquam pharetra
-            ornare lectus at venenatis. Etiam ullamcorper lorem eget ligula
-            commodo malesuada. Maecenas ultrices auctor ultricies. Donec in
-            pellentesque est, ac dignissim metus. Nulla auctor sem non odio
-            hendrerit aliquet non quis lacus. Phasellus lacinia est ut enim
-            ultrices suscipit. Nam libero nisi, blandit vitae metus sit amet,
-            accumsan scelerisque mi. Morbi vestibulum, massa quis scelerisque
-            ultrices, mi lacus placerat orci, vel facilisis diam nibh quis ante.
-            Aenean fermentum, enim in convallis sagittis, arcu lectus semper
-            felis, sed blandit felis sem sed lectus. Cras lacinia rutrum est,
-            vitae aliquam eros feugiat in. In maximus mauris accumsan arcu
-            bibendum elementum in ac elit. Curabitur eu leo ultricies, vulputate
-            elit eu, iaculis massa. Aliquam in aliquam nisi, sit amet molestie
-            magna. Nam a dignissim felis. Phasellus in mi tempus, egestas nibh
-            interdum, tristique est. Sed dapibus mauris at porta tempus. Nam in
-            fermentum tortor. Praesent blandit turpis enim, at pulvinar libero
-            ornare ac. Sed dignissim ex ac odio viverra dapibus. Aliquam ut
-            sollicitudin eros.
+            Sou <strong>Mestre</strong> em Intervenções <strong>Cognitivo-Comportamentais</strong> em <strong>Psicologia Clínica</strong> e da <strong>Saúde</strong>, formada na Faculdade de Psicologia e Ciências da Educação da <strong>Universidade de Coimbra</strong>. Após concluir o meu estágio curricular no Centro de Saúde de Condeixa-a-Nova e realizar diversas experiências de voluntariado, atualmente estou a desenvolver o meu Ano Profissional Júnior na Clínica do Paião. <br/> Desde cedo, a psicologia clínica tornou-se a minha verdadeira paixão. Ajudar os outros a superar desafios e promover o seu bem-estar é profundamente gratificante para mim. No meu trabalho, aplico o modelo cognitivo-comportamental de 2ª e 3ª geração, conhecido pela sua eficácia em tratar uma ampla gama de questões, incluindo ansiedade, depressão, stress e dificuldades comportamentais. <br/> Os meus colegas consideram-me uma pessoa organizada, focada e dedicada. No meu tempo livre, gosto de praticar hipismo, ver filmes, ler, socializar com amigos e jogar. <br/> Estou disponível para apoiar quem procura melhorar a sua saúde mental e enfrentar desafios da vida. Sinta-se à vontade para me contactar para marcar uma consulta ou explorar oportunidades de colaboração.
           </p>
         </div>
       </div>
