4f6cf5220f883eeefce4c02180ef5f0a0247e878
Lucas Oriental dos Santos <lucasoriental16@gmail.com>
2024-08-07 11:08:23 +0100

    main
    Modal de experiências profissionais

---
+++ src/components/sections/professionalExperiences/professionalExperiencesModal.jsx
@@ -0,0 +1,55 @@
+import { useEffect } from "react";
+import XIconSvg from "../../../assets/icon_X";
+
+export default function ProfessionalExperiencesModal({ item, onClose }) {
+  useEffect(() => {
+    document.body.classList.add("modal-open");
+
+    return () => {
+      document.body.classList.remove("modal-open");
+    };
+  }, []);
+  return (
+    <div className="modal-background">
+      <div className="modal-container">
+        <div className="modal-header">
+          <button
+            className="modal-close-button"
+            onClick={onClose}
+          >
+            <XIconSvg size={25} strokeWidth={3} color="white" />
+          </button>
+        </div>
+        <div className="modal-body-experiencia-profissional">
+          <div className="modal-experiencia-profissional-div">
+            <img
+              src={item.imagem}
+              alt="logo da empresa"
+              className="modal-experiencia-profissional-imagem"
+            />
+            <canvas className="modal-experiencia-profissional-barra-divisoria" />
+            <div className="modal-experiencia-profissional-div-info">
+              <p className="modal-experiencia-profissional-titulo">
+                {item.titulo}
+              </p>
+              <p className="modal-experiencia-profissional-empresa">
+                {item.empresa}
+              </p>
+              <p className="modal-experiencia-profissional-tempo">
+                {item.tempo}
+              </p>
+            </div>
+          </div>
+          <div className="modal-experiencia-profissional-coluna-info">
+            <p className="modal-experiencia-profissional-responsabilidades-titulo">
+              Responsabilidades:
+            </p>
+            <p className="modal-experiencia-profissional-responsabilidades">
+              {item.responsabilidades}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
