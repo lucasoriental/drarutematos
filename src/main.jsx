02483ecf2a61fda0959283be20d6ef66b921af9c
lucasoriental <lucasoriental16@gmail.com>
2025-05-12 14:01:56 +0100

    Removing a wrong importation

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './components/styles/index.css'
 import "./components/styles/tailwind.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
